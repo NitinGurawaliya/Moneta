7a6d8ddf3277b26b850b54d0f1524167d57cd3d8
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-15 22:54:55 +0530

    Create Appbar.jsx

---
+++ paytm-main-main/frontend/src/components/Appbar.jsx
@@ -0,0 +1,19 @@
+export const Appbar=()=>{
+    return <div className="shadow h-14 w-full flex justify-between bg-slate-400">
+        <div className="ml-4 flex flex-col justify-left h-full mt-4">
+            PayTM App
+        </div>
+        <div className="flex">
+        <div className="flex flex-col justify-right h-full mr-4 mt-4">
+            Hello
+        </div>
+        <div className="rounded-full h-12 w-12 flex justify-center mr-2 ml-2 bg-slate-500">
+            <div className="flex flex-col justify-center h-full text-xl">
+                U
+            </div>
+        </div>
+        </div>
+
+    </div>
+
+}
