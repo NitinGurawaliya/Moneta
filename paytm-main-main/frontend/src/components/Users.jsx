b4679b88f2b5e5192a47993140d38175ade4939c
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-06-20 22:12:20 +0530

    Update Users.jsx

--- paytm-main-main/frontend/src/components/Users.jsx
+++ paytm-main-main/frontend/src/components/Users.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react"
 import { Button } from "./Button"
+import axios from "axios"
+import { useEffect } from "react"
+import {useNavigate} from 'react-router-dom';
+
+
 
 export const Users = () => {
     // Replace with backend call
-    const [users, setUsers] = useState([{
-        firstName: "Nitin",
-        lastName: "Gurawaliya",
-        _id: 1
-    }]);
+    const [users, setUsers] = useState([]);
+    const[filter,setFilter]= useState("");
 
+    useEffect(()=>{
+        axios.get("http://localhost:3002/api/v1/user/bulk?filter="+filter)
+        .then(response=>{
+            setUsers(response.data.user)
+        })
+    },[filter])
     return <>
         <div className="font-bold mt-6 text-lg">
             Users
         </div>
-        <div className="my-2">
+        <div onChange={(e)=>{
+            setFilter(e.target.value)
+        }} className="my-2">
             <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
         <div>
@@ -23,6 +33,8 @@ export const Users = () => {
 }
 
 function User({user}) {
+    const navigate= useNavigate();
+
     return <div className="flex justify-between">
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
@@ -38,7 +50,9 @@ function User({user}) {
         </div>
 
         <div className="flex flex-col justify-center h-ful">
-            <Button label={"Send Money"} />
+            <Button onClick={(e)=>{
+                navigate("/sendmoney?id="+user._id+"&name= "+user.firstName); 
+            }} label={"Send Money"} />
         </div>
     </div>
 }
