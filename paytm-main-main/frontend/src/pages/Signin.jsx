d9f77c2a27ef32d133de8034cf247e9c52789b64
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-07-21 05:13:35 +0530

    Update Signin.jsx

--- paytm-main-main/frontend/src/pages/Signin.jsx
+++ paytm-main-main/frontend/src/pages/Signin.jsx
@@ -33,7 +33,7 @@ export const Signin = () => {
 
 
     return <div className="bg-slate-300 h-screen flex justify-center">
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-center">  
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign in"} />
         <Subheading  label={"Enter your info to access your account"} />
