3b9d6594dd8a92985622a8fc6b95315b0549c5f3
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-11 23:24:55 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+// backend/index.js
+const dotenv = require('dotenv')
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1", rootRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
