04eea239fb1df1bb4dcaf2c7ad376e02270b0971
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-11 23:42:10 +0530

    Update Balance.jsx

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -15,7 +15,7 @@ export function Balance() {
     }
 
     try {
-      const response = await axios.get("http://localhost:3000/api/v1/account/balance", {
+      const response = await axios.get(`${BACKEND_URL}api/v1/account/balance`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
