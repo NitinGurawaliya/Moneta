1e6478d4e436d93717fd041f73dc2bebdb5512f2
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2024-08-11 23:46:57 +0530

    Update Balance.jsx

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -2,6 +2,8 @@
 
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config"
+
 
 export function Balance() {
   const [balance, setBalance] = useState("");
